96128d9975140d99be477824a44138220c16496f
Hiroshi Sato <looks-like-percussionist@outlook.com>
2019-06-08 06:01:49 +0900

    commit

--- 2D_Resnet/main.py
+++ 2D_Resnet/main.py
@@ -3,7 +3,6 @@ import glob
 import os
 import subprocess
 
-import HandDetection as hd
 import cv2
 import numpy as np
 import resnet
@@ -47,13 +46,6 @@ def main(opt):
         subprocess.call('mkdir tmp', shell=True)
         subprocess.call('ffmpeg -loglevel warning -i {} -r 5 tmp/image_%05d.jpg'.format(vpath), shell=True)
         images = sorted(glob.glob('tmp/*.jpg'))
-        if opt.only_hand:
-            print('convert to masked images')
-            for im in tqdm(images):
-                frame = cv2.imread(im)
-                maskedframe, _ = hd.detect(frame)
-                cv2.imwrite(im, maskedframe)
-            print('complete convert images')
 
         print('extract {}\'s DeepFeatrue'.format(vname))
 
@@ -64,24 +56,14 @@ def main(opt):
             subprocess.call('mkdir {}'.format(outpath), shell=True)
 
         savename = os.path.join(outpath, vname + '.npy')
-        # savename1 = os.path.join(outpath, 'layer1', vname + '.npy')
-        # savename2 = os.path.join(outpath, 'layer2', vname + '.npy')
-        # savename3 = os.path.join(outpath, 'layer3', vname + '.npy')
-        # savename4 = os.path.join(outpath, 'layer4', vname + '.npy')
         np.save(savename, outputs)
-        # np.save(savename1, ops1)
-        # np.save(savename2, ops2)
-        # np.save(savename3, ops3)
-        # np.save(savename4, ops4)
         subprocess.call('rm -rf tmp', shell=True)
 
 
 def input_image(im_paths, model):
-    # 画像のリサイズ，テンソル化
     transform = transforms.Compose([transforms.Resize(224),
                                     transforms.CenterCrop(224),
                                     transforms.ToTensor(),
-                                    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
                                     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
                                     ])
     dataset = TransDataset(im_paths, transform=transform)
@@ -95,24 +77,8 @@ def input_image(im_paths, model):
     for idx, input in enumerate(data_loader):
         if torch.cuda.is_available():
             input = input.to('cuda')
-        # output, op1, op2, op3, op4 = model(input)
         output = model(input)
         outputs.append(output[0].to('cpu').data.numpy())
-    #     ops1.append(op1[0].to('cpu').data.numpy())
-    #     ops2.append(op2[0].to('cpu').data.numpy())
-    #     ops3.append(op3[0].to('cpu').data.numpy())
-    #     ops4.append(op4[0].to('cpu').data.numpy())
-    # outputs = np.array(outputs)
-    # ops1 = np.array(ops1)
-    # ops2 = np.array(ops2)
-    # ops3 = np.array(ops3)
-    # ops4 = np.array(ops4)
-    # print(outputs.shape)
-    # print(ops1.shape)
-    # print(ops2.shape)
-    # print(ops3.shape)
-    # print(ops4.shape)
-    # return outputs, ops1, ops2, ops3, ops4
     return outputs
 
 
---
+++ README.md
@@ -0,0 +1,29 @@
+# Video Alignment Process
+
+This is Video alignment process.
+
+
+
+# Requirement
+- torch
+- torchvision
+- numpy
+- opencv-python
+- joblib
+
+# Usage
+
+1. Feature Extract
+
+Extract videos' feature by 2D CNN and 3D CNN.
+
+This 3D CNN is a customized version of [Hara's work](https://github.com/kenshohara/video-classification-3d-cnn-pytorch).
+
+You can download pretrained model in his repository.
+
+1. Alignmnet
+
+Align videos using extracted features.
+
+
+You can measure the accuracy by `evaluate_labels.py`
